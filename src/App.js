41d90607fc02f4036bbb3656a151dd9856fdf202
LoraBel07 <91973134+LoraBel07@users.noreply.github.com>
2022-07-24 23:01:47 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-
 import './App.css';
+import { useEffect } from 'react';
+
 
 function App() {
+  const MY_ID = "f8d5c297";
+  const MY_KEY = "261b2e6790ec7580369fa3062441ca66";
+
+  useEffect( async () => {
+    const response = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=f8d5c297&app_key=261b2e6790ec7580369fa3062441ca66`);
+    console.log(response);
+  })
+
+
+
   return (
     <div className="App">
-      
+      <p>RECIPES</p>
     </div>
   );
 }
